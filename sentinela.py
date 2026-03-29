9dd5a71b0761cc83556088cde600176d6655df96
miltom8557gmail <miltom.jose8557@gmail.com>
2026-03-29 16:28:34 -0300

    Forja Total

--- sentinela.py
+++ sentinela.py
@@ -5,13 +5,12 @@ app = Flask(__name__)
 
 @app.route('/forjar', methods=['POST'])
 def forjar():
-    print("ORDEM RECEBIDA DO MESTRE MILTON!")
-    # COMANDOS QUE O TERMUX VAI EXECUTAR
-    os.system("git add .")
-    os.system("git commit -m 'Forja disparada pelo App'")
-    os.system("git push origin main -f")
-    print("FRAMES ENVIADOS AO GITHUB!")
-    return "Ordem executada", 200
+    print("\n[!] ORDEM RECEBIDA DO MESTRE MILTON!")
+    # Sincroniza e envia ao GitHub
+    os.system("git add . && git commit -m 'Forja via App' && git push origin main -f")
+    # Vigia e abre o vídeo
+    os.system("gh run watch && termux-vibrate && termux-open video.mp4")
+    return "Sucesso", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
