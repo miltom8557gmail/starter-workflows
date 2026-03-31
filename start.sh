17a71ae9bf70c6b4df6981df948cfdc7eebd627e
miltom8557gmail <miltom.jose8557@gmail.com>
2026-03-31 02:14:32 -0300

    [🚀] ATUALIZAÇÃO AUTOMÁTICA: 31/03/2026 02:14:32

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
-fuser -k 8081-8089/tcp > /dev/null 2>&1 ; python sentinela.py
+#!/bin/bash
+# Mata processos antigos
+fuser -k 8081-8089/tcp > /dev/null 2>&1
+echo "[🔥] INICIANDO SISTEMA AKAME..."
+cd ~/Imperio_IA
+# Inicia o servidor Python
+python sentinela.py & 
+# Inicia o Sentinela de Backup Automático (A cada 5 min)
+echo "[🛰️] BACKUP AUTOMÁTICO ATIVADO EM SEGUNDO PLANO."
+while true; do
+    git add .
+    git commit -m "Auto-Sinc Akame" && git push origin main
+    sleep 300
+done &
