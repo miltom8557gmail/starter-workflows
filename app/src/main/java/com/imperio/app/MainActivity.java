23ffd9a110a26b7ed2c5e599d19bd9dde1b0a829
miltom8557gmail <miltom8557gmail@gmail.com>
2026-04-01 11:37:02 -0300

    💎 [ESTABILIZAÇÃO] Injetando código Core do App

--- app/src/main/java/com/imperio/app/MainActivity.java
+++ app/src/main/java/com/imperio/app/MainActivity.java
@@ -1,11 +1,28 @@
 package com.imperio.app;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText pinInput = findViewById(R.id.pinInput);
+        Button btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pin = pinInput.getText().toString();
+                if (pin.equals("3650") || pin.equalsIgnoreCase("Milton")) {
+                    Toast.makeText(MainActivity.this, "ACESSO CONCEDIDO", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "PIN INCORRETO", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
