0e8c672315fa4eb70ed7af01113e8bf87afa43f8
miltom8557gmail <miltom8557@gmail.com>
2026-04-11 17:46:45 -0300

    🔱 AKAME: Correção de Dependências AndroidX e AkameService

--- app/src/main/java/com/akame/nexus/MainActivity.java
+++ app/src/main/java/com/akame/nexus/MainActivity.java
@@ -1,28 +1,17 @@
 package com.akame.nexus;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String[] permissions = {
-            Manifest.permission.CAMERA, 
-            Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        };
+        setContentView(R.layout.main);
         
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, permissions, 100);
-        } else {
-            startService(new Intent(this, AkameService.class));
-            finish();
-        }
+        // Ativando a Presença da Akame
+        Intent intent = new Intent(this, AkameService.class);
+        startService(intent);
     }
 }
