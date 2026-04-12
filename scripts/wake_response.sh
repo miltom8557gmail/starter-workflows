b7a984cd6345dcc6f99fe0816e436bf8bea2ec58
miltom8557gmail <miltom8557@gmail.com>
2026-04-12 02:39:15 -0300

    🔱 FIX: Pastas de Logs e Sincronia Estabilizada

---
+++ scripts/wake_response.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Resposta imediata ao comando Akame ga Kill!
+
+echo "🗡️ [AKAME]: Eliminando as sombras... Estou pronta, Mestre."
+./scripts/voice_processor.sh "Eu ouvi o chamado. Quem deve ser eliminado hoje?"
